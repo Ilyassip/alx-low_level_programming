abb6ede7369ccc2c07ec60eceb77851972c1f9b6
Ilyassip <ilyassefawzi9@gmail.com>
2023-08-11 06:23:14 -0700

    programming

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,14 +8,23 @@
 int main(void)
 {
 	int i;
+	int j;
 
-	for (i = 10; i < 20; i++)
+	for (i = 0; i <= 99; i++)
 	{
-		putchar((i % 10) + '0');
-		if (i != 19)
+		for (j = i + 1; j <= 99; j++)
 		{
-			putchar(',');
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
 			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+
+			if (!(i == 98 && j == 99))
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
